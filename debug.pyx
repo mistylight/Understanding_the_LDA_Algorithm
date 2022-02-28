f4a1a2efaf3e0a7afe351f67c2c2601ba9104772
mistylight <mistylight.cs@gmail.com>
2022-02-28 13:45:33 -0800

    topic-word visualization

--- debug.pyx
+++ debug.pyx
@@ -5,11 +5,13 @@ import random
 from collections import defaultdict, OrderedDict
 from types import SimpleNamespace
 from tqdm.notebook import tqdm
+import time
 
 
 # === corpus loading ===
 cdef class NeurIPSCorpus:
-    cdef list docs
+    cdef np.ndarray docs
+    cdef np.ndarray doc_lengths
     cdef dict word2id
     cdef int num_docs
     cdef int num_topics
@@ -18,12 +20,19 @@ cdef class NeurIPSCorpus:
 
     def __init__(self, state_dict_path):
         cdef dict state_dict = json.load(open(state_dict_path))
-        self.docs = state_dict["docs"]
+        cdef list docs = state_dict["docs"]
         self.word2id = state_dict["word2id"]
         self.num_topics = state_dict["num_topics"]
         self.max_doc_length = state_dict["max_doc_length"]
-        self.num_docs = len(self.docs)
+        self.num_docs = len(docs)
         self.num_words = len(self.word2id)
+        self.docs = np.zeros([self.num_docs, self.max_doc_length], dtype=int)
+        self.doc_lengths = np.zeros([self.num_docs], dtype=int)
+        cdef int i, j
+        for i in range(self.num_docs):
+            for j in range(len(docs[i])):
+                self.docs[i, j] = docs[i][j]
+            self.doc_lengths[i] = len(docs[i])
         print(
             "num_docs:", self.num_docs, 
             "num_topics:", self.num_topics, 
@@ -47,6 +56,8 @@ cdef Hyperparameters hparams = Hyperparameters(
     gibbs_sampling_max_iters=10,
 )
 
+start_time = time.time()
+
 # === initialization ===
 print("Initializing...")
 cdef np.ndarray n_doc_topic = np.zeros([corpus.num_docs, corpus.num_topics], dtype=float) # n_m^(k)
@@ -61,12 +72,18 @@ cdef np.ndarray p_topic_word = np.zeros([corpus.num_topics], dtype=float)
 cdef np.ndarray phi = np.zeros([corpus.num_docs, corpus.num_topics], dtype=float)
 cdef np.ndarray theta = np.zeros([corpus.num_topics, corpus.num_words], dtype=float)
 
+topics = np.random.randint(0, corpus.num_topics, 
+    size=(corpus.num_docs, corpus.max_doc_length))
 for doc_i in range(corpus.num_docs):
-    for j, word_j in enumerate(corpus.docs[doc_i]):
-        topic_ij = np.random.randint(0, corpus.num_topics)
+    topics_i = np.random.randint(0, corpus.num_topics, 
+    for j in range(corpus.doc_lengths[doc_i]):
+        word_j = corpus.docs[doc_i, j]
+        topic_ij = topics[doc_i, j]
         n_doc_topic[doc_i, topic_ij] += 1
         n_topic_word[topic_ij, word_j] += 1
         z_doc_word[doc_i, j] = topic_ij
+
+print(time.time() - start_time)
 
 # === Gibbs sampling ===
 print("Gibbs sampling...")
---
+++ visualize.py
@@ -0,0 +1,32 @@
+import plotly
+from plotly.colors import n_colors
+import plotly.graph_objects as go
+
+def visualize_topic_word(words, probs, max_prob=0.05):
+    """
+    words: List[List[str]], shape (num_topics, num_top_words)
+        The top X words for each topic.
+    probs: List[List[float]], shape (num_topics, num_top_words)
+        Probabilities for the top X words for each topics.
+    max_prob: float in (0, 1)
+        The min probability to be shown as the deepest color.
+    """
+    LIGHT_GREEN = "rgb(238, 246, 237)"
+    AVOCADO_GREEN = "rgb(55, 125, 34)"
+    GREY = "rgb(220, 220, 220)"
+    cmap = n_colors(LIGHT_GREEN, AVOCADO_GREEN, 10, colortype="rgb")
+    
+    header = [f"Topic {i:02d}" for i in range(10)]
+    colors = [[cmap[int(min(p_ij / max_prob, 1) * 9)] for p_ij in p_i] for p_i in probs]
+
+    fig = go.Figure(
+        go.Table(
+            header=dict(values=header, fill_color=GREY),
+            cells=dict(values=words, fill_color=colors)
+        )
+    )
+    fig.update_layout(
+        title_text=f"Top words for the {len(words)} topics",
+        width=1350,
+    )
+    fig.show()
